51a861759f9be504613c62d8afe3be0dc580226c
kamibushiele <84666539+kamibushiele@users.noreply.github.com>
2025-05-25 23:00:36 +0900

    ゲームクラスをgamejsに移動

--- js/game.js
+++ js/game.js
@@ -15,7 +15,7 @@ class ThrowButton {
                 case 1: return "btn-warning"; // おしい
                 case 2: return "btn-success"; // 命中
             }
-        }else{
+        } else {
             return "btn-secondary";
         }
     }
@@ -49,7 +49,7 @@ class ThrowItem {
     classText() {
         if (this.status != null) {
             return "throw-block throwed";
-        }else{
+        } else {
             return "throw-block unthrowed";
         }
     }
@@ -59,15 +59,14 @@ class ThrowItem {
         }
         if (this.status === status) {
             this.status = null; // 既に同じステータスの場合は未投に戻す
-        }else{
+        } else {
             this.status = status; // ステータスを更新
         }
-        console.log(`Target: ${this.target}, Status: ${this.status}, Points: ${this.points}, Buttons: ${this.buttons.map(btn => btn.text).join(", ")}`);
     }
 }
 
 // ゲームクラス
-export class Game {
+class Game {
     constructor() {
         /** @type {Target_t[]} */
         const targets = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, "B"];
@@ -77,5 +76,50 @@ export class Game {
     }
     get totalScore() {
         return this.throws.reduce((acc, throwItem) => acc + throwItem.points, 0);
+    }
+}
+
+export class GameApp {
+    constructor() {
+        /** @type {Game[]} */
+        this.games = [];
+        this.newGame(); // 初期化時に新しいゲームを開始
+    }
+    get currentGame() {
+        if (this.games.length === 0) {
+            return null; // ゲームがない場合はnullを返す
+        }
+        return this.games[this.games.length - 1]; // 最後のゲームを返す
+    }
+    newGame() {
+        const currentGame = this.currentGame;
+        if (currentGame && !currentGame.gameFinished) {
+            this.currentGame.gameFinished = true; // 現在のゲームを終了
+        }
+        this.games.push(new Game());
+    }
+    clearGame() {
+        this.games.pop(); // 最後のゲームを削除
+        if (this.games.length === 0) {
+            this.newGame(); // 新しいゲームを開始
+            return;
+        } else {
+            this.currentGame.gameFinished = false; // 現在のゲームを未完了に設定
+        }
+    }
+    log() {
+        const finishedGames = this.games.filter(game => game.gameFinished);
+        let historyText = ""
+        if (finishedGames.length === 0) {
+            historyText = "履歴がありません";
+        } else {
+            historyText += finishedGames.map((game) => game.totalScore).join(", ");
+            historyText += `\n\nゲーム数: ${finishedGames.length}`;
+            historyText += `\n最高スコア: ${Math.max(...finishedGames.map(game => game.totalScore))}`;
+            historyText += `\n最低スコア: ${Math.min(...finishedGames.map(game => game.totalScore))}`;
+            const averageScore = finishedGames.reduce((acc, game) => acc + game.totalScore, 0) / finishedGames.length;
+            historyText += `\n平均スコア: ${averageScore.toFixed(2)}`;
+        }
+        return historyText;
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,49 +1,8 @@
 import Alpine from 'https://unpkg.com/alpinejs@3.x.x/dist/module.esm.js'
-import { Game } from './game.js';
+import { GameApp } from './game.js';
 
 const appGame = {
-    /** @type {Game[]} */
-    games: [],
-    get currentGame() {
-        if (this.games.length === 0) {
-            return null; // ゲームがない場合はnullを返す
-        }
-        return this.games[this.games.length - 1]; // 最後のゲームを返す
-    },
-    init() {
-        this.newGame(); // 初期化時に新しいゲームを開始
-    },
-    newGame() {
-        const currentGame = this.currentGame;
-        if (currentGame && !currentGame.gameFinished) {
-            this.currentGame.gameFinished = true; // 現在のゲームを終了
-        }
-        this.games.push(new Game());
-    },
-    clearGame() {
-        this.games.pop(); // 最後のゲームを削除
-        if (this.games.length === 0) {
-            this.newGame(); // 新しいゲームを開始
-            return;
-        } else {
-            this.currentGame.gameFinished = false; // 現在のゲームを未完了に設定
-        }
-    },
-    log(){
-        const finishedGames = this.games.filter(game => game.gameFinished);
-        let historyText = ""
-        if (finishedGames.length === 0) {
-            historyText = "履歴がありません";
-        } else {
-            historyText += finishedGames.map((game) => game.totalScore).join(", ");
-            historyText += `\n\nゲーム数: ${finishedGames.length}`;
-            historyText += `\n最高スコア: ${Math.max(...finishedGames.map(game => game.totalScore))}`;
-            historyText += `\n最低スコア: ${Math.min(...finishedGames.map(game => game.totalScore))}`;
-            const averageScore = finishedGames.reduce((acc, game) => acc + game.totalScore, 0) / finishedGames.length;
-            historyText += `\n平均スコア: ${averageScore.toFixed(2)}`;
-        }
-        return historyText;
-    }
+    game: new GameApp(),
 }
 
 Alpine.data("app", () => appGame);
